356f8f5663487c683fd0cafb7bb37939429ad454
chegde <chegde@foxmail.com>
2019-01-21 09:42:10 +0800

    fix: fine

---
+++ weatherIndex.js
@@ -0,0 +1,29 @@
+	const getWeather = require('./weather');
+	const notify = require('./notify');
+	const env = require('./env');
+	const fs = require('fs');
+
+	const system_log = console.log;
+	console.log = function (...params) {
+	  system_log(...params);
+	  const now = new Date();
+	  fs.appendFile('service.log', now.toLocaleString() + ' ' + params + '\n', (err) => {
+		if (err) throw err;
+	  });
+	}
+
+	const [myHour, myMinute] = env.time.split(':');
+	try {
+	  setInterval(() => {
+		const now = new Date();
+		if (now.getHours() == myHour && now.getMinutes() == myMinute) {
+		  getWeather().then((ans) => {
+			console.log('today weather: ' + ans);
+			notify(ans);
+		  });
+		}
+	  }, 1000 * 60);
+	} catch (e) {
+	  console.log(e);
+	}
+
